c32c420aebe28ddc7ab31b8b61e0690f1e00f667
Matt <mbrowne108@gmail.com>
2022-10-04 01:19:09 -0400

    Add animated trail for ship

--- script.js
+++ script.js
@@ -11,38 +11,26 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
-const space = new Image();
-space.src = "images/space.png";
-ctx.imageSmoothingEnabled = false;
 
 window.addEventListener("keydown", keyDown);
 window.addEventListener("keyup", keyUp);
-document.addEventListener('mousemove', (e) => {
+window.addEventListener('mousemove', (e) => {
     mouse.x = e.clientX
     mouse.y = e.clientY
 })
-// document.addEventListener("click", (e) => {
-//     if (ship.torpedoes.length < torpMax) {
-//         ship.torpedoes.push({
-//             x: ship.x + 4/3 * ship.height / 1.7 * Math.cos(ship.a),
-//             y: ship.y - 4/3 * ship.height / 1.7 * Math.sin(ship.a),
-//             xv: torpSpeed * Math.cos(ship.a) / FPS,
-//             yv: -torpSpeed * Math.sin(ship.a) / FPS
-//         })
-//     }
-// });
 window.addEventListener("mousedown", (e) => {
     ship.firing = true;
 });
 
 window.addEventListener('mouseup', (e) => {
-    ship.firing = false
+    ship.firing = false;
 })
-
 
 canvas.addEventListener('wheel', (e) => {
     if (e.wheelDelta >= 0 ) {
-        scale += 0.1 * scale
+        if (scale < 5) {
+            scale += 0.1 * scale
+        }
     } else {
         if (scale > 0.1) {
             scale -= 0.1 * scale
@@ -103,7 +91,8 @@ const ship = {
 const torpedo = {
     el: document.getElementById("torpedo"),
     height: ship.height / 2,
-    width: ship.width / 2
+    width: ship.width / 2,
+    shadow: 'rgb(248,58,37)'
 }
 
 const sun = {
@@ -296,7 +285,7 @@ function drawPlanet(planet) {
 
     // Hover over planet
     if ((mouse.x > planet.x + cameraOffset.x && mouse.x < planet.x + planet.width + cameraOffset.x) && (mouse.y > planet.y + cameraOffset.y && mouse.y < planet.y + planet.height + cameraOffset.y)) {
-        ctx.fillStyle = "red";
+        ctx.fillStyle = "white";
         ctx.font = "15px serif";
         ctx.fillText(planet.name, planet.x + cameraOffset.x, planet.y + cameraOffset.y)
     }
@@ -323,6 +312,7 @@ function drawShip() {
     ctx.drawImage(ship.thrusting ? ship.elThrust : ship.el, ship.x - (ship.width / 2) + cameraOffset.x, ship.y - (ship.height / 1.7) + cameraOffset.y, ship.width, ship.height)
     ctx.restore()
 
+    // Move ship
     if (ship.thrusting) {
         ship.thrust.x += shipThrust * Math.cos(ship.a) / FPS;
         ship.thrust.y -= shipThrust * Math.sin(ship.a) / FPS;
@@ -335,8 +325,6 @@ function drawShip() {
         ship.thrust.x -= 3 * friction * ship.thrust.x / FPS;
         ship.thrust.y -= 3 * friction * ship.thrust.y / FPS;
     }
-
-    // Move ship
     ship.x += ship.thrust.x;
     ship.y += ship.thrust.y;
 }
@@ -344,7 +332,7 @@ function drawShip() {
 function drawTorpedoes() {
     for (var i = 0; i < ship.torpedoes.length; i++) {
         ctx.shadowColor = torpedo.shadow
-        ctx.shadowBlur = torpedo.height * scale
+        ctx.shadowBlur = torpedo.height * 3
         ctx.drawImage(torpedo.el, ship.torpedoes[i].x + cameraOffset.x - (torpedo.width / 2), ship.torpedoes[i].y + cameraOffset.y - (torpedo.height / 2), ship.width / 2, ship.height / 2)
         ctx.shadowBlur = 0
     }
@@ -365,8 +353,33 @@ function drawTorpedoes() {
     }
 }
 
+const motionTrailLength = 50
+const positions = []
+function storeLastPosition(xPos, yPos) {
+    positions.push({
+        x: xPos,
+        y: yPos
+    })
+    if (positions.length > motionTrailLength) {
+        positions.shift()
+    }
+}
+
+function drawMotionTrail() {
+    for (let i = 0; i < positions.length; i++) {
+        let ratio = (i + 1) / positions.length
+        ctx.beginPath();
+        ctx.shadowColor = 'rgba(101,150,240)'
+        ctx.shadowBlur = 10 * scale
+        ctx.arc(positions[i].x + cameraOffset.x, positions[i].y + cameraOffset.y, (ship.height / 10) * ratio, 0, 2 * Math.PI, true);
+        ctx.fillStyle = `rgba(101,150,240,${ratio / 2})`;
+        ctx.fill();
+        ctx.shadowBlur = 0;
+    }
+}
+
 // Game Loop
-setInterval(() => {
+function loop() {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
 
@@ -376,13 +389,14 @@ setInterval(() => {
     cameraOffset.x = canvas.width / 2 - ship.x
     cameraOffset.y = canvas.height / 2 - ship.y
 
-    // Background?
-    // ctx.drawImage(space, camera.x - cameraOffset.x, camera.y - cameraOffset.y, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
     drawStars();
     drawSun();
     planets.forEach(drawPlanet);
+    drawMotionTrail();
     drawShip();
     drawTorpedoes();
+
+    storeLastPosition(ship.x, ship.y)
 
     // ctx.fillStyle = "red";
     // ctx.font = "15px serif";
@@ -391,7 +405,10 @@ setInterval(() => {
     // ctx.fillText(`Crosshair: ${mouse.x}, ${mouse.y}`, 5, 45);
     // ctx.fillText(`Offset: ${Math.round(cameraOffset.x)}, ${Math.round(camera.y)}`, 5, 60);
     // ctx.fillText(`Canvas: ${Math.round(canvas.width)}, ${Math.round(canvas.width)}`, 5, 75);
-    // ctx.fillText(`Space: ${Math.round(space.width)}, ${Math.round(space.height)}`, 5, 90);
+    // ctx.fillText(`Sun: ${sun.x}, ${sun.y}`, 5, 90);
     // ctx.fillText(`Scale: ${scale}`, 5, 105);
 
-}, 1000 / FPS)
+    requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
